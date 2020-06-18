d1c7f1654229971b88e27b8215d4e78718d19bc7
iClusterDev <cluster.coredev@gmail.com>
2020-06-18 22:15:51 +0100

    matchArray class created

--- memjs/src/scripts/app.js
+++ memjs/src/scripts/app.js
@@ -1,1 +1,51 @@
-export default function () {}
+class MatchArray {
+  constructor() {
+    this.array = [];
+  }
+
+  add(value) {
+    if (this.array.length >= 0 && this.array.length < 2) {
+      this.array.push(value);
+    }
+  }
+
+  clear() {
+    this.array = [];
+  }
+
+  isFull() {
+    return this.array.length === 2;
+  }
+
+  match() {
+    if (this.isFull()) {
+      const [value1, value2] = this.array;
+      return value1 === value2;
+    } else {
+      return false;
+    }
+  }
+}
+
+export default function () {
+  // 0 - initialize the match array
+  const matchArray = new MatchArray();
+
+  // 1 - on loaded populate the gameboard with game cards
+  // 2 - on card click
+  //      flip the card
+  //      store the card value into the match array
+  //      if match array length === 2 check for match
+  //        if match
+  //          increase scores (max 8)
+  //          if score === 8
+  //            you win
+  //          else
+  //            remove event listener from cards
+  //            clear the match array
+  //        else
+  //          reflip the cards
+  //          clear the match array
+  //      else
+  //        keep going
+}
